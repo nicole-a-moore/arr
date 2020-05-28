bf6d0559790864e618d88eeb44f7e120e50eb2e3
nicole-a-moore <nicole.moore@mail.mcgill.ca>
2020-05-28 14:59:41 -0400

    adding arr_calculator

--- R_scripts/08_arr-calculator.R
+++ R_scripts/08_arr-calculator.R
@@ -5,6 +5,7 @@ library(lmodel2)
 library(janitor)
 library(RColorBrewer)
 library(viridis)
+library(nlme)
 
 ## read in files
 intratherm <- read.csv( "./data-processed/arr_sliding-window-output.csv") ## version of the database with only usable data and experienced variation column 
@@ -45,149 +46,140 @@ write.csv(intratherm, "./data-processed/arr_sliding-window-output-with-arr.csv",
 
 
 
-
-
-
 ##get rid of duplicated rows since acclimation temp no longer matters, get rid of negative ARRs
-LSV_ARR_data <- LSV_ARR_data[!duplicated(LSV_ARR_data[,2]),]
-LSV_ARR_data <- subset(LSV_ARR_data, subset = as.numeric(as.character(ARR)) > -0.15)
+intratherm2 <- intratherm %>%
+  filter(!duplicated(intratherm$population_id)) %>%
+  filter(as.numeric(as.character(ARR)) > -0.15)
 
-write.csv(LSV_ARR_data, "~/Documents/SUNDAY LAB/Intratherm/Data sheets/phylo_input.csv")
+write.csv(intratherm2, "~/Documents/SUNDAY LAB/Intratherm/Data sheets/phylo_input.csv")
 
-ggplot(data = cadillac, aes(x = acclim_temp, y = parameter_value, group = population_id, col = LSV)) + 
-  geom_point(size = 1, alpha = 0.5) + geom_errorbar(aes(ymin=parameter_value-error_estimate,
-                                                        ymax=parameter_value+error_estimate), width=0, alpha = 0.5) +
+## colour plot by experienced variation metric 
+ggplot(data = intratherm, aes(x = acclim_temp, y = parameter_value, group = population_id, 
+                              col = experienced_var_mean)) + 
+  geom_point(size = 1, alpha = 0.5) + 
+  geom_errorbar(aes(ymin=parameter_value-error_estimate,ymax=parameter_value+error_estimate),
+                width=0, alpha = 0.5) +
   geom_smooth(method = "lm", se = FALSE, size = 0.4, alpha = 0.1) +
   theme_minimal() + 
-  theme(panel.border = 
-          element_rect(colour = "dimgrey", fill = NA), panel.grid.minor = 
-          element_line(colour = "white"), panel.grid.major = 
-          element_line(colour = "white"), plot.title = element_text(hjust = 0.5)) + 
-  labs(x = "Acclimation temperature (°C)", y = "Upper critical thermal limit (°C)", colour = paste(" Mean", "\n", "experienced", "\n", "variation")) + scale_colour_viridis_c(option = "A")
+  theme(panel.border = element_rect(colour = "dimgrey", fill = NA), 
+        panel.grid.minor = element_line(colour = "white"), panel.grid.major = 
+        element_line(colour = "white"), plot.title = element_text(hjust = 0.5)) + 
+  labs(x = "Acclimation temperature (°C)",y = "Upper critical thermal limit (°C)", 
+       colour = paste(" Mean", "\n", "experienced", "\n", "variation")) +
+  scale_colour_viridis_c(option = "A")
 
 
+## linear model of experienced_var_mean weighted by 1/variance of ARR stope estimate
+## just kidding, scrap this because most ARR don't have an error estimate
+lm_output_all_mean <- intratherm2 %>%
+  drop_na(ARR, experienced_var_mean) %>%
+  do(tidy(lm(ARR ~ experienced_var_mean, data = .), conf.int = TRUE))
 
+lm_output_all_top<- intratherm2 %>%
+  drop_na(ARR, experienced_var_mean) %>%
+  do(tidy(lme(ARR ~ experienced_var_mean, random = ~1|order/family/genus/species,
+              data = ., na.action= NULL), conf.int = TRUE))
 
-## linear model of LSV weighted by 1/variance of ARR stope estimate
-lm_output_all_mean <- LSV_ARR_data %>%
-  drop_na(ARR,LSV_meanSD) %>%
-  do(tidy(lm(ARR ~ LSV_meanSD, data = .), conf.int = TRUE))
-
-lm_output_all_top<- LSV_ARR_data %>%
-  drop_na(ARR,LSV_meanSD) %>%
-  do(tidy(lme(ARR ~ LSV_meanSD, random = ~1|order/family/genus/species, 
-              data = data, na.action= NULL), conf.int = TRUE))
-
-ggplot(LSV_ARR_data, aes(x = LSV_meanSD, y = ARR, col = abs(latitude))) + geom_point() + geom_smooth(method = "lm", color = "black") + geom_errorbar(aes(ymin=ARR-std.error, ymax=ARR+std.error), width=0) + labs(x = "Mean lifespan specific variation (°C)", y = "Acclimation response ratio", col = "Absolute latitude (°N/S)" ) + scale_colour_viridis_c(option = "A") + geom_line(y = predict(y = ))
-
+ggplot(intratherm2, aes(x = experienced_var_mean, y = ARR, col = abs(latitude))) + 
+  geom_point() + 
+  geom_smooth(method = "lm", color = "black") + 
+  geom_errorbar(aes(ymin=ARR-std.error, ymax=ARR+std.error), width=0) + 
+  labs(x = "Mean experienced variation (°C)", y = "Acclimation response ratio", 
+       col = "Absolute latitude (°N/S)" ) + 
+  scale_colour_viridis_c(option = "A")
 
 
 
 ## write to file to make map + taxonomic breakdown from
-write.csv(LSV_ARR_data, "~/Documents/SUNDAY LAB/Intratherm/Data sheets/LSV_ARR_output_critical.csv", row.names = FALSE)
+write.csv(intratherm2, "./data-processed/arr_map-and-model-input.csv", row.names = FALSE)
 
 ## lm with max sd
-lm_output_all_max<- LSV_ARR_data %>%
-  drop_na(ARR,LSV_max) %>%
-  do(tidy(lm(ARR ~ LSV_max, data = .), conf.int = TRUE))
+lm_output_all_max<- intratherm2 %>%
+  drop_na(ARR,experienced_var_max) %>%
+  do(tidy(lm(ARR ~ experienced_var_max, data = .), conf.int = TRUE))
 
-ggplot(LSV_ARR_data, aes(x = LSV_max, y = ARR)) + geom_point() + geom_smooth(method = "lm") 
+ggplot(intratherm2, aes(x = experienced_var_max, y = ARR)) + geom_point() + geom_smooth(method = "lm") 
 
 
 ## lm by class mean
-lm_output_class_mean <- LSV_ARR_data %>%
-  drop_na(ARR,LSV_mean) %>%
-  group_by(class.y) %>%
-  do(tidy(lm(ARR ~ LSV_mean, data = .), conf.int = TRUE))
+lm_output_class_mean <- intratherm2 %>%
+  drop_na(ARR,experienced_var_mean) %>%
+  group_by(class) %>%
+  mutate(experienced_var_mean = mean(experienced_var_mean)) %>%
+  do(tidy(lm(ARR ~ experienced_var_mean, data = .), conf.int = TRUE))
 
-ggplot(LSV_ARR_data, aes(x = LSV_mean, y = ARR, col = class.y)) + geom_point() + geom_smooth(method = "lm") + labs(col = "Class")
+ggplot(intratherm2, aes(x = experienced_var_mean, y = ARR, col = class)) + 
+  geom_point() + 
+  geom_smooth(method = "lm") + 
+  labs(col = "Class")
 
 
 ## SMA regression:
-sma_output_mean <- LSV_ARR_data %>%
-  drop_na(ARR,LSV_mean) %>%
-  lmodel2(ARR ~ LSV_mean, data = .) %>%
+sma_output_mean <- intratherm2 %>%
+  drop_na(ARR,experienced_var_mean) %>%
+  lmodel2(ARR ~ experienced_var_mean, data = .) %>%
   plot("SMA") 
 
 
-###### test whether sd and not sliding window works 
-sd_vals <- c()
-
-i <- 1
-while (i < 409) {
-  name <- paste("~/Documents/SUNDAY LAB/collection_location_tmax_series/loc", i, ".csv", sep = "")
-  loc <- read.csv(name)
-  
-  standard_dev <- sd(loc$temp_value, na.rm = TRUE)
-  sd_vals <- append(sd_vals, standard_dev, after = length(sd_vals))
-  i = i+1
-}
-
-study_locs <- read.csv("~/Documents/SUNDAY LAB/Intratherm/Data sheets/locations_unique.csv")
-study_locs$sd <- sd_vals
-
-
-
-
-sd_data <- left_join(ARR_data, study_locs, by = c("latitude", "longitude"))  
-sd_data <- sd_data[!duplicated(sd_data[,2]),]
-sd_data <- subset(sd_data, subset = !is.na(ARR))
+###### EXPLORE!! ------------------------------------------
 
 ## does sd increase with increasing latitude as expected?
-ggplot(sd_data, aes(x = abs(latitude), y = sd, col = (latitude >0))) + geom_point() 
+ggplot(intratherm2, aes(x = abs(latitude), y = sd_cumulative, col = (latitude >0))) +
+  geom_point() 
 
 ## does sd at location over all years predict ARR? aka without sliding window
-ggplot(sd_data, aes(x = sd, y = ARR, col = abs(latitude))) + geom_point()+ geom_smooth(method = "lm", se = TRUE, col = "black") + labs(x = "Standard deviation of monthly average TMAX", y = "Acclimation response ratio (ARR)") + scale_colour_viridis_c(option = "A") + geom_errorbar(aes(ymin=ARR-std.error, ymax=ARR+std.error), width=0) 
+ggplot(intratherm2, aes(x = sd_cumulative, y = ARR, col = abs(latitude))) + 
+  geom_point() + 
+  geom_smooth(method = "lm", se = TRUE, col = "black") +
+  labs(x = "Standard deviation of monthly average TMAX", y = "Acclimation response ratio (ARR)") + 
+  scale_colour_viridis_c(option = "A") + 
+  geom_errorbar(aes(ymin=ARR-std.error, ymax=ARR+std.error), width=0) 
 
-lm_output_all_sd <- sd_data %>%
-  drop_na(ARR,sd) %>%
-  do(tidy(lm(ARR ~ sd, data = .), conf.int = TRUE))
+lm_output_all_sd <- intratherm2 %>%
+  drop_na(ARR, sd_cumulative) %>%
+  do(tidy(lm(ARR ~ sd_cumulative, data = .), conf.int = TRUE))
 
 
 ## does ARR vary predictably with latitude 
-ggplot(sd_data, aes(x = abs(latitude), y = ARR)) + geom_point() + geom_smooth(method = "lm")
+ggplot(intratherm2, aes(x = abs(latitude), y = ARR)) + 
+  geom_point() + 
+  geom_smooth(method = "lm")
 
 
 ## does ARR vary predictably with maximum tmax value for a species? 
-max_param <- merged %>%
+max_param <- intratherm %>%
   group_by(population_id) %>%
-  arrange( desc(parameter_value) ) %>% 
+  arrange(desc(parameter_value)) %>% 
   slice(1) 
 
-ggplot(max_param, aes(x = parameter_value, y = ARR)) + geom_point() + geom_smooth(method = "lm")
+ggplot(max_param, aes(x = parameter_value, y = ARR)) + 
+  geom_point() + 
+  geom_smooth(method = "lm")
 
 
 
 ### 
-ggplot(LSV_ARR_data, aes(x = lifespan_days, y = LSV_mean)) + geom_point() + geom_smooth(method = "lm")
+ggplot(intratherm2, aes(x = lifespan_days, y = experienced_var_mean)) + 
+  geom_point() + 
+  geom_smooth(method = "lm")
 
-### LSV vs latitude 
-ggplot(LSV_ARR_data, aes(x = abs(latitude), y = LSV_meanIQR)) + geom_point() + geom_smooth(method = "lm", col = "black") + labs(x = "abs(latitude)", y = "Mean range between 0.025 quantile and 0.975 quantile") 
+### experienced_var vs latitude 
+ggplot(intratherm2, aes(x = abs(latitude), y = experienced_var_mean)) + 
+  geom_point() + 
+  geom_smooth(method = "lm", col = "black") + 
+  labs(x = "abs(latitude)", y = "experienced_var_mean") 
 
 
 ### body size
-lm_output_bs <- LSV_ARR_data %>%
-  drop_na(ARR,maximum_body_size_svl_hbl_cm) %>%
+lm_output_bs <- intratherm2 %>%
+  drop_na(ARR, maximum_body_size_svl_hbl_cm) %>%
   do(tidy(lm(ARR ~ maximum_body_size_svl_hbl_cm, data = .), conf.int = TRUE))
 
-ggplot(LSV_ARR_data, aes(x = maximum_body_size_svl_hbl_cm, y = ARR)) + geom_point() + geom_smooth(method = "lm")
+ggplot(intratherm2, aes(x = maximum_body_size_svl_hbl_cm, y = ARR)) + 
+  geom_point() + 
+  geom_smooth(method = "lm")
 
 
-ggplot(LSV_ARR_data, aes(x = abs(latitude), y = lifespan_days)) + geom_point() + geom_smooth(method = "lm")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ggplot(intratherm2, aes(x = abs(latitude), y = lifespan_days)) + 
+  geom_point() + 
+  geom_smooth(method = "lm")
